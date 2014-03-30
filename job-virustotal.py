da5ad0d913f090e9e728a7006f39965e92af2aa9
Michael Boman <michael@michaelboman.org>
2014-03-30 20:38:14 +0200

    Added exception handling.

--- job-virustotal.py
+++ job-virustotal.py
@@ -46,47 +46,52 @@ if Config().virustotal.proxy:
              'https': Config().virustotal.proxy}
 
 while True:
-    for (sampleno, sample) in \
-        enumerate(db.fs.files.find({'virustotal': {'$exists': False}})):
-        try:
-            logger.info('[%s] Processing sample %s' % (sampleno,
-                        sample['sha256']))
-            key = {'sha256': sample['sha256']}
-            parameters = {'resource': sample['sha256'],
-                          'apikey': api_key}
-
-            logger.debug('[%s] Analysing' % sampleno)
-            r = requests.post(url, data=parameters, proxies=proxy)
-
-            VTjson = None
-            logger.debug('[%s] Response headers: %s' % (sampleno,
-                         r.headers))
-            logger.debug('[%s] Response content: %s' % (sampleno,
-                         r.content))
+    try:
+        for (sampleno, sample) in \
+            enumerate(db.fs.files.find({'virustotal': {'$exists': False}})):
             try:
-                VTjson = clean_data(r.json())
-            except Exception:
+                logger.info('[%s] Processing sample %s' % (sampleno,
+                            sample['sha256']))
+                key = {'sha256': sample['sha256']}
+                parameters = {'resource': sample['sha256'],
+                              'apikey': api_key}
+    
+                logger.debug('[%s] Analysing' % sampleno)
+                r = requests.post(url, data=parameters, proxies=proxy)
+    
+                VTjson = None
+                logger.debug('[%s] Response headers: %s' % (sampleno,
+                             r.headers))
+                logger.debug('[%s] Response content: %s' % (sampleno,
+                             r.content))
                 try:
-                    VTjson = clean_data(json.loads(r.text))
+                    VTjson = clean_data(r.json())
                 except Exception:
-                    logger.debug('[%s] Unknown response: %s'
-                                 % (sampleno, r.content))
+                    try:
+                        VTjson = clean_data(json.loads(r.text))
+                    except Exception:
+                        logger.debug('[%s] Unknown response: %s'
+                                     % (sampleno, r.content))
+    
+                if VTjson:
+                    if VTjson['response_code'] == 1:
+                        logger.debug('[%s] Storing results into MongoDB'
+                                     % sampleno)
+                        db.fs.files.update(key,
+                                {'$set': {'virustotal': VTjson}},
+                                upsert=True)
+                        logger.info('[%s] Metadata updated' % sampleno)
+                    else:
+                        logger.warn('[%s] File Does Not Exist in VirusTotal'
+                                     % sampleno)
+            except Exception, e:
+    
+                logger.exception(e)
+                pass
+    except Exception, e:
 
-            if VTjson:
-                if VTjson['response_code'] == 1:
-                    logger.debug('[%s] Storing results into MongoDB'
-                                 % sampleno)
-                    db.fs.files.update(key,
-                            {'$set': {'virustotal': VTjson}},
-                            upsert=True)
-                    logger.info('[%s] Metadata updated' % sampleno)
-                else:
-                    logger.warn('[%s] File Does Not Exist in VirusTotal'
-                                 % sampleno)
-        except Exception, e:
+        logger.exception(e)
 
-            logger.exception(e)
-            pass
 
     logger.info('Sleeping %s minutes' % SLEEPTIME)
     time.sleep(SLEEPTIME * 60)
